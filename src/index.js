20d5d63b170124b29fc2091cfbbf5ce24f45167d
Jillian Werner <jillianlwerner@gmail.com>
2021-05-05 17:04:22 -0400

    Create basic data-grabbing functions; move API key to separate module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 import "./style.scss";
+import { getAPIKey } from "/apiKey.js";
 
-console.log("JS is working");
+const convertTemp = function (temp, unit = "Kelvin") {
+  const unitConversion = {
+    Celsius: Number(temp) - 273.15,
+    Fahrenheit: (Number(temp) * 9) / 5 - 459.67,
+    Kelvin: Number(temp),
+  };
+
+  let convertedTemp = Math.round(unitConversion[unit]);
+  return convertedTemp;
+};
+
+const showWeather = function (weather) {
+  console.log(weather);
+  console.log(convertTemp(weather.main.temp, "Fahrenheit"));
+};
+
+const getWeatherData = async function (location) {
+  try {
+    const data = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${getAPIKey()}`,
+      {
+        mode: "cors",
+      }
+    );
+
+    const weather = await data.json();
+    showWeather(weather);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Promise / Then version of async / await function from above
+// const getWeatherDataPromiseThen = function (location) {
+//   fetch(
+//     `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${secretKey.getKey()}`,
+//     {
+//       mode: "cors",
+//     }
+//   )
+//     .then((response) => response.json())
+//     .then((jsonData) => showWeather(jsonData))
+//     .catch((err) => console.log(err));
+// };
+
+getWeatherData("San Francisco");
